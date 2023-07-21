fa07149da1189ddf558ad632bcfb5ce2e703a91f
Jason Zheng <jason.zheng@yale.edu>
2023-07-20 22:10:54 -0400

    Attempt to implement project modals

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import PokerHandApp from "../assets/poker-7174110.png";
 import WorkInProgress from "../assets/work-in-progress.png";
 import { Col, Row } from "react-bootstrap";
 import Plus from "../assets/plus.svg";
+import ReactModal from 'react-modal';
 
 const projects = [
         {
@@ -11,7 +12,8 @@ const projects = [
             technicals: "Used: Poker-odds API, React, JS, HTML, CSS",
             description: "Gives probability of your poker hand winning based off the cards in your hand and those on the table",
             imgUrl: PokerHandApp,
-            gitHub: "https://github.com/jz1459",
+        gitHub: "https://github.com/jz1459",
+            id: 1,
         },
         {
             title: "To-Do App w/ Randomizer",
@@ -19,6 +21,7 @@ const projects = [
             description: "In Progress",
             imgUrl: WorkInProgress,
             gitHub: "https://github.com/jz1459",
+            id: 2,
         },
         {
             title: "Taste of China Website",
@@ -26,6 +29,7 @@ const projects = [
             description: "In Progress",
             imgUrl: WorkInProgress,
             gitHub: "https://github.com/jz1459",
+            id: 3,
         },
         {
             title: "Fantasy Football App",
@@ -33,6 +37,7 @@ const projects = [
             description: "In Progress",
             imgUrl: WorkInProgress,
             gitHub: "https://github.com/jz1459",
+            id: 4,
         },
 ];
     
@@ -60,132 +65,57 @@ function ProjectCarousel(props) {
 
 
 function ProjectCard() {
-    return (
-        <Row>
-            <div className="twelve columns collapsed">
-                <div id="proj-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                    <div className="columns proj-item">
-                        <div className="proj-itemWrapper">
-                            <a href="#modal-01" title="">
-                                <img src={projects[0].imgUrl} alt="" />
-                                <div className="proj-overlay">
-                                    <div className="proj-title">
-                                        <h4>{projects[0].title}</h4>
-                                    </div>
-                                </div>
-                                <div className="proj-plusSign">
-                                    <img src={Plus} alt="Plus Sign"></img>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="columns proj-item">
-                            <div className="proj-itemWrapper">
-                                <a href="#modal-01" title="">
-                                    <img src={projects[1].imgUrl} alt="" />
-                                    <div className="proj-overlay">
-                                        <div className="proj-title">
-                                            <h4>{projects[1].title}</h4>
-                                        </div>
-                                    </div>
-                                    <div className="proj-plusSign">
-                                        <img src={Plus} alt="Plus Sign"></img>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="columns proj-item">
-                            <div className="proj-itemWrapper">
-                                <a href="#modal-01" title="">
-                                    <img src={projects[2].imgUrl} alt="" />
-                                    <div className="proj-overlay">
-                                        <div className="proj-title">
-                                            <h4>{projects[2].title}</h4>
-                                        </div>
-                                    </div>
-                                    <div className="proj-plusSign">
-                                        <img src={Plus} alt="Plus Sign"></img>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="columns proj-item">
-                            <div className="proj-itemWrapper">
-                                <a href="#modal-01" title="">
-                                    <img src={projects[3].imgUrl} alt="" />
-                                    <div className="proj-overlay">
-                                        <div className="proj-title">
-                                            <h4>{projects[3].title}</h4>
-                                        </div>
-                                    </div>
-                                    <div className="proj-plusSign">
-                                        <img src={Plus} alt="Plus Sign"></img>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Row>
-    );
-};
+    const [isOpen, setIsOpen] = useState(false);
+    const [modalData, setModalData] = useState(null);
 
-
-function ProjectPopUps() {
+    const handleClick = (data) => {
+        setIsOpen(true);
+        setModalData(data);
+    };
     
     return (
-        <div className="proj-descriptions">
-            <div id="modal-01" className="popup-modal mfp-hide">
-                <img className="scale-with-grid" src={projects[0].imgUrl} alt="" />
-                <div className="description-box">
-                    <h4>{projects[0].title}</h4>
-                    <p>{projects[0].description}</p>
-                    <span className="categories">{projects[0].technicals}</span>
+        <>
+            <Row>
+                {projects.map(project => (
+                    <Col size={12} sm={6} md={6}>
+                        <div className="proj-item" key={project.id}>
+                            <div className="proj-wrap">
+                                <a
+                                    onClick={() => {
+                                        handleClick(project)
+                                    }}>
+                                    <img src={project.imgUrl} alt="Project Image" />
+                                </a>
+                                <div className="proj-overlay">
+                                    <div className="proj-title">
+                                        <h4>{project.title}</h4>
+                                    </div>
+                                    <div className="proj-plusSign">
+                                        <img src={Plus} alt="Plus Sign" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                ))}
+            </Row>
+            <ReactModal
+                isOpen={isOpen}
+                onRequestClose={() => setIsOpen(false)}
+            >
+                <div className="proj-txtx">
+                    <h4>{modalData.description}</h4>
+                    <span>{modalData.technicals}</span>
                 </div>
-                <div className="link-box">
-                    <a href={projects[0].gitHub} target="_blank">View on GitHub</a>
-                    <a className="popup-modal-dismiss">Close</a>
+                <div className="proj-close">
+                    <button onClick={() => isOpen(false)}>X</button>
                 </div>
-            </div>
-            <div id="modal-02" className="popup-modal mfp-hide">
-                <img className="scale-with-grid" src={projects[1].imgUrl} alt="" />
-                <div className="description-box">
-                    <h4>{projects[1].title}</h4>
-                    <p>{projects[1].description}</p>
-                    <span className="categories">{projects[1].technicals}</span>
-                </div>
-                <div className="link-box">
-                    <a href={projects[0].gitHub} target="_blank">View on GitHub</a>
-                    <a className="popup-modal-dismiss">Close</a>
-                </div>
-            </div>
-            <div id="modal-03" className="popup-modal mfp-hide">
-                <img className="scale-with-grid" src={projects[2].imgUrl} alt="" />
-                <div className="description-box">
-                    <h4>{projects[2].title}</h4>
-                    <p>{projects[2].description}</p>
-                    <span className="categories">{projects[2].technicals}</span>
-                </div>
-                <div className="link-box">
-                    <a href={projects[0].gitHub} target="_blank">View on GitHub</a>
-                    <a className="popup-modal-dismiss">Close</a>
-                </div>
-            </div>
-            <div id="modal-04" className="popup-modal mfp-hide">
-                <img className="scale-with-grid" src={projects[3].imgUrl} alt="" />
-                <div className="description-box">
-                    <h4>{projects[3].title}</h4>
-                    <p>{projects[3].description}</p>
-                    <span className="categories">{projects[3].technicals}</span>
-                </div>
-                <div className="link-box">
-                    <a href={projects[3].gitHub} target="_blank">View on GitHub</a>
-                    <a className="popup-modal-dismiss">Close</a>
-                </div>
-            </div>
-        </div>
+            </ReactModal>
+        </>
     );
 };
+
+
 export default ProjectCarousel;
 
-export { ProjectCard , ProjectPopUps};
+export { ProjectCard };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import ProjectCarousel, {ProjectCard, ProjectPopUps } from "../components/ProjectCards";
+import ProjectCarousel, {ProjectCard} from "../components/ProjectCards";
 // import colorSharp2 from "../assets/.png";
 import 'animate.css';
 // import TrackVisibility from 'react-on-screen';
+import PokerHandApp from "../assets/poker-7174110.png";
+import WorkInProgress from "../assets/work-in-progress.png";
+import Plus from "../assets/plus.svg";
 
+const projects = [
+        {
+            title: "Poker Hand App",
+            technicals: "Used: Poker-odds API, React, JS, HTML, CSS",
+            description: "Gives probability of your poker hand winning based off the cards in your hand and those on the table",
+            imgUrl: PokerHandApp,
+            gitHub: "https://github.com/jz1459",
+        },
+        {
+            title: "To-Do App w/ Randomizer",
+            technicals: "Design & Development",
+            description: "In Progress",
+            imgUrl: WorkInProgress,
+            gitHub: "https://github.com/jz1459",
+        },
+        {
+            title: "Taste of China Website",
+            technicals: "Design & Development",
+            description: "In Progress",
+            imgUrl: WorkInProgress,
+            gitHub: "https://github.com/jz1459",
+        },
+        {
+            title: "Fantasy Football App",
+            technicals: "Design & Development",
+            description: "In Progress",
+            imgUrl: WorkInProgress,
+            gitHub: "https://github.com/jz1459",
+        },
+];
 function Projects(){
     return (
         <section className="projects" id="projects">
@@ -32,7 +65,7 @@ function Projects(){
                                     {/* <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}> */}
                                     <Tab.Pane eventKey="first">
                                         <ProjectCard />
-                                        <ProjectPopUps />
+                                        {/* <ProjectPopUps /> */}
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="second">
                                         <ProjectCarousel />
